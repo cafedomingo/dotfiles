ab378fa61779ab2c0e8b577a3a9151229bc0569a
J. Patrick Sunday <j.patrick.sunday@gmail.com>
2017-01-02 09:41:47 -0800

    use consistent syntax for sourcing scripts

--- sh/bash/completion.bash
+++ sh/bash/completion.bash
@@ -3,7 +3,7 @@
 if  has_homebrew && [[ -f $(brew --prefix)/share/bash-completion/bash_completion ]]; then
   # use brew installed `homebrew/versions/bash-completion2`
   # includes any other brew installed completions - brew, gem, git, rake, launchctl, etc.
-  . $(brew --prefix)/share/bash-completion/bash_completion;
+  source $(brew --prefix)/share/bash-completion/bash_completion;
 elif [[ -f /etc/bash_completion ]]; then
-  . /etc/bash_completion;
+  source /etc/bash_completion;
 fi;
