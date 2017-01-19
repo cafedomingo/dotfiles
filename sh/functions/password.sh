e591da17ef5bbce6f20993c60f315dd9734cd7f9
J. Patrick Sunday <j.patrick.sunday@gmail.com>
2017-01-19 00:09:25 -0800

    give the password shell function some options

--- sh/functions/password.sh
+++ sh/functions/password.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env sh
 
 # generate a random string of letters, digits and symbols and copy to clipboard
-# $1: password length (default: 40)
 function password()
 {
-  ! [ $# -eq 0 -o $# -eq 1 -a "$1" -eq "$1" ] 2>/dev/null && echo "$1 is not a valid length" && return 1
+  local usage="\
+usage: password [-s] [-n length]
+  -s      exclude symbols/punctuation
+  -n=NUM  specify password length"
+  local length=40
+  local chars="[:alnum:][:punct:]"
 
-  local length=$([ -z $1 ] && echo 40 || echo $1)
-  LC_ALL=C tr -dc "[:alnum:][:punct:]" < /dev/urandom | head -c $length | pbcopy
+  while getopts n:s opt
+  do
+     case "$opt" in
+        n) length=$OPTARG;;
+        s) chars=${chars%"[:punct:]"};;
+        [?]) echo $usage && return 1;;
+     esac
+  done
+
+  ! [ "$length" -eq "$length" ] 2>/dev/null && echo "error: $length is not a valid length" && return 1
+  [ $length -lt 1 ] && echo "error: length must be greater than 0" && return 1
+
+  LC_ALL=C tr -dc "$chars" < /dev/urandom | head -c $length | pbcopy
 }
