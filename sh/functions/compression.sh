7fa52c775ae9e59df92c17cf1410463d8ee43431
J. Patrick Sunday <j.patrick.sunday@gmail.com>
2017-04-01 14:45:40 -0700

    consolidate multiple compression functions into a single shrink function

--- sh/functions/compression.sh
+++ sh/functions/compression.sh
@@ -28,84 +28,66 @@ function extract() {
   fi
 }
 
-# compress the given file or directory using 7z "ultra settings"
-function 7zm() {
-  [ -z $1 ] || [ ! -e $1 ] && echo 'specify a file or directory to compress' && return 1
-
-  local location=$(dirname $1)
-  local filename=$(basename $1)
-
-  local cmd=(
-    # 7za is a stand-alone executable.
-    # 7za handles less archive formats than 7z, but does not need any others.
+# compress target file or directory using one of multiple compression options
+function shrink() {
+  local usage="\
+usage: shrink [-7|r|z] [file|directory]
+  -7      compress using 7-zip
+  -r      compress using rar
+  -z      compress using zip (default)"
+  local z7=(
     7za
-    a                 # add/create
-    -t7z              # 7z archive
-    -m0=lzma          # lzma method
-    -mx=9             # level of compression = 9 (Ultra)
-    -mfb=64           # number of fast bytes for LZMA = 64
-    -md=32m           # dictionary size = 32 megabytes
-    -ms=on            # solid archive = on
-    '-xr!*.DS_Store'  # exclude .DS_Store files
+    a                       # add/create
+    -t7z                    # 7z archive
+    -m0=lzma                # lzma method
+    -mx=9                   # level of compression = 9 (Ultra)
+    -mfb=64                 # number of fast bytes for LZMA = 64
+    -md=32m                 # dictionary size = 32 megabytes
+    -ms=on                  # solid archive = on
+    '-xr!*.DS_Store'        # exclude .DS_Store files
   )
-
-  ${cmd[@]} "$location/$filename.7z" "$1"
-}
-
-# individually compress each file/directory in the current directory into it's own archive using 7z "ultra settings"
-function 7zm_cwd() {
-  for entry in ./*; do
-    7zm $entry
-  done
-}
-
-# compress the given file or directory using rar max compression
-function rar5() {
-  [ -z $1 ] || [ ! -e $1 ] && echo 'specify a file or directory to compress' && return 1
-
-  local location=$(dirname $1)
-  local filename=$(basename $1)
-
-  local cmd=(
+  local rar=(
     rar
-    a                 # add/create
-    -s                # solid archive
-    -m5               # max compression
-    -md64             # 64mb dictionary
-    -ma5              # rar 5.x formar
-    '-x!*.DS_Store'   # exclude .DS_Store files
+    a                       # add/create
+    -s                      # solid archive
+    -m5                     # max compression
+    -md64                   # 64mb dictionary
+    -ma5                    # rar 5.x formar
+    '-x!*.DS_Store'         # exclude .DS_Store files
   )
-
-  ${cmd[@]} "$location/$filename.rar" "$1"
-}
-
-# individually compress each file/directory in the current directory into it's own archive using rar max compression
-function rar5_cwd() {
-  for entry in ./*; do
-    rar5 $entry
-  done
-}
-
-# compress the given file or directory using zip max compression
-function z9() {
-  [ -z $1 ] || [ ! -e $1 ] && echo 'specify a file or directory to compress' && return 1
-
-  local location=$(dirname $1)
-  local filename=$(basename $1)
-
-  local cmd=(
+  local zip=(
     zip
     -9                      # maximum compression
     -r                      # recurse into directories
     --exclude='*.DS_Store'  # exclude .DS_Store files
   )
 
-  ${cmd[@]} "$location/$filename.zip" "$1"
+  local cmd=("${zip[@]}")
+  local extension='7z'
+
+  while getopts 7rz opt
+  do
+     case $opt in
+        7) extension='7z'   &&  cmd=("${z7[@]}")    ;;
+        r) extension='rar'  &&  cmd=("${rar[@]}")   ;;
+        z) extension='zip'  &&  cmd=("${zip[@]}")   ;;
+        [?]) echo $usage && return 1                ;;
+     esac
+  done
+
+  # validate target file or directory
+  local target=$@[$OPTIND]
+  [ -z $target ] || [ ! -e $target ] && echo "'$target' is not a valid file or directory" && return 1
+
+  local location=$(dirname $target)
+  local filename=$(basename $target)
+
+  ${cmd[@]} "$location/$filename.$extension" "$target"
 }
 
-# individually compress each file/directory in the current directory into it's own archive using zip max compression
-function z9_cwd() {
+# individually compress each file/directory in the current directory
+function shrink_cwd() {
   for entry in ./*; do
-    z9 $entry
+    shrink $@ $entry
   done
 }
