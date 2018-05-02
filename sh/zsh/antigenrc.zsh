c11d3b2c341a57839a0c17e71d01e34e72f6439c
Patrick <33767+cafedomingo@users.noreply.github.com>
2018-05-01 23:19:07 -0700

    remove unused prompt sections

--- sh/zsh/antigenrc.zsh
+++ sh/zsh/antigenrc.zsh
@@ -36,23 +36,11 @@ SPACESHIP_PROMPT_ORDER=(
   dir           # Current directory section
   git           # Git section (git_branch + git_status)
   hg            # Mercurial section (hg_branch  + hg_status)
-  package       # Package version
-  node          # Node.js section
-  ruby          # Ruby section
-  xcode         # Xcode section
-  swift         # Swift section
-  golang        # Go section
-  docker        # Docker section
-  aws           # Amazon Web Services section
-  venv          # virtualenv section
-  pyenv         # Pyenv section
-  dotnet        # .NET section
   exec_time     # Execution time
   line_sep      # Line break
   battery       # Battery level and status
-  vi_mode       # Vi-mode indicator
   jobs          # Background jobs indicator
-  time          # Time stampts section
+  time          # Time stamp section
   exit_code     # Exit code section
   char          # Prompt character
 )
