7421737b6b67950608c922e30fac6657b27aba03
J. Patrick Sunday <j.patrick.sunday@gmail.com>
2018-12-15 11:27:31 -0800

    standardize current directory statement

--- sh/functions.sh
+++ sh/functions.sh
@@ -3,11 +3,7 @@
 # load RVM into a shell *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-if [[ -n "${BASH_SOURCE[0]}" ]]; then
-  location=$(dirname "${BASH_SOURCE[0]}")
-else
-  location=$(dirname "${(%):-%x}")
-fi
+location=$(dirname "$([ -z "${BASH_SOURCE[0]}" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}")")
 
 # load functions
 for file in "$location"/functions/*.sh; do
