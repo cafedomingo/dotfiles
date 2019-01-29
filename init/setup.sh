882d466910a275353099f16f76b88e54ac078121
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2019-01-28 20:56:15 -0800

    Fix OS detection

--- init/setup.sh
+++ init/setup.sh
@@ -2,7 +2,7 @@
 
 location=$(dirname "$([ -z "${BASH_SOURCE[0]}" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}")")
 
-if [[ $(uname -s) == Darwin ]]; then
+if [ $(uname -s) = "Darwin" ]; then
   source "$location"/macos/prefs.sh
   source "$location"/macos/install.sh
 fi;
