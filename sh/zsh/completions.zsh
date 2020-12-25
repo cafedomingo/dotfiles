5c96a5f7de66426820b4b584b5219445dc4195c3
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2020-12-24 21:01:53 -0800

    wrap zsh scripts in anonymous functions to scope local variables

--- sh/zsh/completions.zsh
+++ sh/zsh/completions.zsh
@@ -1,29 +1,27 @@
 #!/usr/bin/env zsh
 
-completions="$(dirname "${(%):-%x}")"/plugins
+function {
+  local completions="$(dirname "${(%):-%x}")"/plugins
 
-# Load completions
-fpaths=(
-  "$completions/zsh-completions"                # standard zsh completions
-)
+  # Load completions
+  local fpaths=(
+    "$completions/zsh-completions"                # standard zsh completions
+  )
 
-# completions for homebrew packages
-if command -v brew &>/dev/null; then
-  fpaths=( "$(brew --prefix)/share/zsh/site-functions" $fpaths)
-fi
+  # completions for homebrew packages
+  if command -v brew &>/dev/null; then
+    fpaths=( "$(brew --prefix)/share/zsh/site-functions" $fpaths)
+  fi
 
-for search_path in "${fpaths[@]}"; do
-  [[ -d $search_path ]] \
-  && [[ $fpath != *$search_path* ]] \
-  && fpath=( $search_path $fpath )
-done
+  for search_path in "${fpaths[@]}"; do
+    [[ -d $search_path ]] \
+    && [[ $fpath != *$search_path* ]] \
+    && fpath=( $search_path $fpath )
+  done
 
-# Case insensitive path-completion
-zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
+  # Case insensitive path-completion
+  zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
 
-autoload -Uz compinit
-compinit
-
-# cleanup
-unset completions
-unset fpaths
+  autoload -Uz compinit
+  compinit
+}
