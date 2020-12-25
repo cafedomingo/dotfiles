bb9cc9303ba3c969f21cc47b70d0bbf8a09763bb
Patrick Sunday <cafedomingo@users.noreply.github.com>
2020-12-24 21:01:53 -0800

    wrap zsh scripts in anonymous functions to scope local variables

--- sh/zsh/plugins.zsh
+++ sh/zsh/plugins.zsh
@@ -1,10 +1,9 @@
 #!/usr/bin/env zsh
 
-plugins="$(dirname "${(%):-%x}")"/plugins
+function {
+	local plugins="$(dirname "${(%):-%x}")"/plugins
 
-source $plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
-source $plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-# cleanup
-unset plugins
+	source $plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+	source $plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+	source $plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+}
