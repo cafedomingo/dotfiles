a09d618b02815485fc59b345f63b9942a94a9e6c
Patrick <33767+cafedomingo@users.noreply.github.com>
2020-12-24 17:47:18 -0800

    confirm brew is available before loading zsh completions

--- sh/zsh/completions.zsh
+++ sh/zsh/completions.zsh
@@ -5,8 +5,13 @@ completions="$(dirname "${(%):-%x}")"/plugins
 # Load completions
 fpaths=(
   "$completions/zsh-completions"                # standard zsh completions
-  "$(brew --prefix)/share/zsh/site-functions"   # completions for homebrew packages
 )
+
+# completions for homebrew packages
+if command -v brew &>/dev/null; then
+  fpaths=( "$(brew --prefix)/share/zsh/site-functions" $fpaths)
+fi
+
 for search_path in "${fpaths[@]}"; do
   [[ -d $search_path ]] \
   && [[ $fpath != *$search_path* ]] \
