468388082033688379e8291609b726da48d4038c
Patrick Sunday <cafedomingo@users.noreply.github.com>
2020-12-30 09:23:07 -0800

    consolidate zsh completions into single file

--- sh/zsh/completions.zsh
+++ sh/zsh/completions.zsh
@@ -22,3 +22,20 @@ function {
   autoload -Uz compinit
   compinit
 }
+
+# enable cursor menu selection
+zstyle ':completion:*:*:*:*:*' menu select
+
+# case, hyphen and underscore insensitive path-completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
+
+# complete . and .. special directories
+zstyle ':completion:*' special-dirs true
+
+# use cashe
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path $ZSH_CACHE_DIR
+
+# show colors on completion suggestions
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
