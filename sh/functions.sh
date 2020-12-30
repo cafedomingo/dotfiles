710be1239b73c65befdb3f05035c8bc66b9c07b6
Patrick Sunday <cafedomingo@users.noreply.github.com>
2020-12-30 09:26:45 -0800

    wrap variable expansion in paths with quotes

--- sh/functions.sh
+++ sh/functions.sh
@@ -6,7 +6,7 @@
 location=$(dirname "$([ -z "${BASH_SOURCE[0]}" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}")")
 
 # load functions
-for file in "$location"/functions/*.sh; do
+for file in "$location/functions/*.sh"; do
   [[ -s $file ]] && source "$file"
 done
 
