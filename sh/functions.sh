c0f77e52361b06235c95769ee4fc95f78412e5d2
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2020-12-30 11:15:17 -0800

    remove rvm

--- sh/functions.sh
+++ sh/functions.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-# load RVM into a shell *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-
 location=$(dirname "$([ -z "${BASH_SOURCE[0]}" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}")")
 
 # load functions
