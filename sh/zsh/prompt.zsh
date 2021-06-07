a551a732c7168aaf2dc2cd0bf468a92d35f96c4c
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2021-06-06 21:36:01 -0700

    use starship prompt in bash and zsh (#3)
    * replace spaceship prompt with starship for zsh
    
    * use starship prompt for bash

--- sh/zsh/prompt.zsh
+++ sh/zsh/prompt.zsh
@@ -1,31 +1,3 @@
 #!/usr/bin/env zsh
 
-## Configure prompt
-# Spaceship documentation: https://denysdovhan.com/spaceship-prompt
-SPACESHIP_DIR_TRUNC=0
-SPACESHIP_DIR_TRUNC_REPO=false
-SPACESHIP_EXEC_TIME_ELAPSED=5
-SPACESHIP_GIT_SYMBOL=''
-SPACESHIP_HOST_PREFIX='@ '
-SPACESHIP_TIME_SHOW=true
-SPACESHIP_TIME_PREFIX=''
-SPACESHIP_TIME_COLOR='#63637B'
-SPACESHIP_PROMPT_ORDER=(
-  user          # Username section
-  host          # Hostname section
-  dir           # Current directory section
-  git           # Git section (git_branch + git_status)
-  hg            # Mercurial section (hg_branch  + hg_status)
-  exec_time     # Execution time
-  line_sep      # Line break
-  battery       # Battery level and status
-  jobs          # Background jobs indicator
-  time          # Time stamp section
-  exit_code     # Exit code section
-  char          # Prompt character
-)
-
-fpath=( "$HOME/.zsh/spaceship" $fpath )
-
-autoload -U promptinit; promptinit
-prompt spaceship
+eval "$(starship init zsh)"
