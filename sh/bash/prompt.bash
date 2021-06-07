34c1657e7648dfcc9a32adc7dbd23c508a485b18
Patrick Sunday <j.patrick.sunday@gmail.com>
2021-06-06 21:36:01 -0700

    use starship prompt in bash and zsh (#3)
    * replace spaceship prompt with starship for zsh
    
    * use starship prompt for bash

--- sh/bash/prompt.bash
+++ sh/bash/prompt.bash
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-GIT_PROMPT_START="${BBlue}\w${Color_Off}"
-GIT_PROMPT_END="\n${IBlack}\@ ${White}➜ ${Color_Off}"
-source "$(dirname "${BASH_SOURCE[0]}")/bash-git-prompt/gitprompt.sh"
+eval "$(starship init bash)"
