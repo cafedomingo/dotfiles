53ee52fa24297a5c16fbd6862f06bb811a4e19ae
Patrick <33767+cafedomingo@users.noreply.github.com>
2023-11-27 17:28:33 -0800

    Use modern less config for syntax coloring

--- sh/env.sh
+++ sh/env.sh
@@ -61,13 +61,7 @@ done
 export MANPATH
 
 # pager highlighting
-export LESS_TERMCAP_mb=$'\e[1;32m'    # begin blinking
-export LESS_TERMCAP_md=$'\e[1;32m'    # begin bold
-export LESS_TERMCAP_me=$'\e[0m'       # end mode
-export LESS_TERMCAP_se=$'\e[0m'       # end standout-mode
-export LESS_TERMCAP_so=$'\e[01;33m'   # begin standout-mode - info box
-export LESS_TERMCAP_ue=$'\e[0m'       # end underline
-export LESS_TERMCAP_us=$'\e[1;4;31m'  # begin underline
+export MANPAGER="less -R --use-color -Dd+G -Du+B"
 
 # default editors
 # http://unix.stackexchange.com/a/4861
