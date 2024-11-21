e5e4b0b92f291ae21f6d2840f5a8e569a8057d8e
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2024-11-20 23:08:21 -0800

    cleanup bash options

--- sh/bash/options.bash
+++ sh/bash/options.bash
@@ -2,10 +2,11 @@
 
 # keep history up to date, across sessions, in realtime
 # http://unix.stackexchange.com/a/48113
-export HISTCONTROL=ignoredups:erasedups
-export HISTSIZE=10000
-export HISTFILESIZE=$HISTSIZE
-type shopt &> /dev/null && shopt -s histappend
-export HISTIGNORE='&:ls:[bf]g:exit:history'
-# save and reload the history after each command finishes
-export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+HISTCONTROL=ignoredups:erasedups
+HISTSIZE=100000
+HISTFILESIZE=$HISTSIZE
+HISTIGNORE='&:ls:[bf]g:exit:history'
+
+# enable history appending and immediate sharing between sessions
+shopt -s histappend 2>/dev/null
+PROMPT_COMMAND="history -a; history -c; history -r; ${PROMPT_COMMAND:-}"
