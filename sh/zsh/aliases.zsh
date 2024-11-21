ca7f571087678490c2b51eff3c14a9d916daa73e
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2024-11-20 22:58:42 -0800

    cleanup zsh scripts

--- sh/zsh/aliases.zsh
+++ sh/zsh/aliases.zsh
@@ -13,14 +13,21 @@ alias -g H='| head'
 alias -g T='| tail'
 alias -g G='| grep'
 alias -g L="| less"
-alias -g NULL="&> /dev/null"
+alias -g N="&> /dev/null"
 
-if is-at-least 4.2.0; then
-  # list whats inside packed files
-  alias -s 7z="7za l "
-  alias -s rar="unrar l"
-  alias -s tar="tar tf "
-  alias -s tar.gz="echo "
-  alias -s tgz="echo "
-  alias -s zip="unzip -l "
-fi
+# Archive viewers (ZSH 4.2.0+)
+is-at-least 4.2.0 && {
+  local -rA archives=(
+      [7z]='7z l'
+      [rar]='unrar l'
+      [zip]='unzip -l'
+      [tar]='tar tf'
+      [tar.gz,tgz]='tar tzf'
+  )
+
+  for ext format in ${(kv)archives}; do
+    for e in ${(s:,:)ext}; do
+      alias -s $e="$format"
+    done
+  done
+}
