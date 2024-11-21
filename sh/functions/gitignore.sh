7fb0bd4db86f6e9e4d395210077881d23126ce42
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2024-11-20 22:08:33 -0800

    cleanup on shell functions

--- sh/functions/gitignore.sh
+++ sh/functions/gitignore.sh
@@ -2,6 +2,6 @@
 
 # generate gitignore file from https://www.gitignore.io
 # https://github.com/joeblau/gitignore.io
-function gi() {
-  curl -sL https://www.gitignore.io/api/"$*@";
+gi() {
+  curl -sL "https://www.gitignore.io/api/$*"
 }
