4288a2cd08770387fd6907da8fa5fafed60c12d3
Patrick Sunday <cafedomingo@users.noreply.github.com>
2025-08-13 12:50:56 -0700

    fix zsh plugin loading order

--- sh/zsh/plugins.zsh
+++ sh/zsh/plugins.zsh
@@ -3,12 +3,12 @@
 () {
   local -r plugin_dir="$(dirname "${(%):-%x}")/plugins"
   local -r plugins=(
-    zsh-syntax-highlighting    # Load this last for best compatibility
-    zsh-history-substring-search
     zsh-autosuggestions
+    zsh-syntax-highlighting
+    zsh-history-substring-search  # Must load after zsh-syntax-highlighting
   )
 
-  for plugin in ${plugins:|tac}; do 
+  for plugin in ${plugins}; do
     source "$plugin_dir/$plugin/$plugin.zsh"
   done
 }
