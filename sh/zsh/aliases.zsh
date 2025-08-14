00e5ffcf1013520c880e4d51db0d9923479c9add
Patrick <33767+cafedomingo@users.noreply.github.com>
2025-08-13 17:17:09 -0700

    add more archive viewer aliases

--- sh/zsh/aliases.zsh
+++ sh/zsh/aliases.zsh
@@ -7,6 +7,7 @@ alias -- -='cd -'
 alias -g ...='../..'
 alias -g ....='../../..'
 
+# global aliases for common pipes and redirections
 alias -g H='| head'
 alias -g T='| tail'
 alias -g G='| grep'
@@ -16,7 +17,14 @@ alias -g N="&> /dev/null"
 # archive viewers
 alias -s 7z='7z l'
 alias -s rar='unrar l'
-alias -s zip='unzip -l'
 alias -s tar='tar tf'
 alias -s tgz='tar tzf'
 alias -s 'tar.gz'='tar tzf'
+alias -s tbz='tar tjf'
+alias -s tbz2='tar tjf'
+alias -s 'tar.bz2'='tar tjf'
+alias -s txz='tar tJf'
+alias -s 'tar.xz'='tar tJf'
+alias -s taz='tar tZf'
+alias -s 'tar.Z'='tar tZf'
+alias -s zip='unzip -l'
