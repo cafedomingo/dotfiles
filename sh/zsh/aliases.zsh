9e39750b9e43650ad128023d7a976a700c7138d7
Patrick Sunday <cafedomingo@users.noreply.github.com>
2025-08-19 12:45:55 -0700

    use als function for zsh suffix aliases

--- sh/zsh/aliases.zsh
+++ sh/zsh/aliases.zsh
@@ -13,13 +13,13 @@ alias -g G='| grep'
 alias -g L="| less"
 alias -g N="&> /dev/null"
 
-# archive viewers
-alias -s 7z='7z l'
-alias -s rar='unrar l'
-alias -s tar='tar tf'
-alias -s taz='tar tZf'
-alias -s tbz='tar tjf'
-alias -s tbz2='tar tjf'
-alias -s tgz='tar tzf'
-alias -s txz='tar tJf'
-alias -s zip='unzip -l'
+# archive viewers (using unified als function)
+alias -s 7z='als'
+alias -s rar='als'
+alias -s tar='als'
+alias -s taz='als'
+alias -s tbz='als'
+alias -s tbz2='als'
+alias -s tgz='als'
+alias -s txz='als'
+alias -s zip='als'
