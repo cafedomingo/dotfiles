2104eddbf54a80bf7bf70c3316d59b5b539830d3
Patrick Sunday <33767+cafedomingo@users.noreply.github.com>
2026-03-31 11:59:08 -0700

    fix history substring search bindings and update to HEAD

--- sh/plugins.zsh
+++ sh/plugins.zsh
@@ -10,4 +10,8 @@
   for plugin in ${plugins}; do
     source "$plugin_dir/$plugin/$plugin.zsh" 2>/dev/null || source "$plugin_dir/$plugin/$plugin.sh"
   done
+
+  # bind up/down to history substring search (must be after plugins are loaded)
+  bindkey '^[[A' history-substring-search-up
+  bindkey '^[[B' history-substring-search-down
 }
